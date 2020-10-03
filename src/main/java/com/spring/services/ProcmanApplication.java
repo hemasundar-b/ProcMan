b801bf2b770799df332f3559dca76e1c5ff967f1
hemasundar <hemasundar@hemasundars-MacBook-Pro.local>
2020-10-03 11:10:12 +0530

    commit

--- src/main/java/com/spring/services/ProcmanApplication.java
+++ src/main/java/com/spring/services/ProcmanApplication.java
@@ -1,4 +1,4 @@
-package com.spring.srevices;
+package com.spring.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
