d4080a4cdf15b0592d5490761c063f114dd8323e
hemasundar <hemasundar@hemasundars-MacBook-Pro.local>
2020-10-03 11:07:27 +0530

    initial commit

---
+++ src/main/java/com/spring/srevices/ProcmanApplication.java
@@ -0,0 +1,13 @@
+package com.spring.srevices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/srevices/ProcmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.srevices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
